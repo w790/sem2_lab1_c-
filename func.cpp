31f7c2a981a142416e2120dea14356ffccf5176f
U-DESKTOP-J0QPH5V\Паха <pasha_w790@bk.ru>
2025-09-05 16:20:42 +0700

    fix bugs:version failed3

--- func.cpp
+++ func.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include "func.h"
 
-#ifndef PROJECT_VERSION
-#define PROJECT_VERSION "1.0.0" // значение по умолчанию
-#endif
-
 int x = 0;
 std::string getversion(){
     return PROJECT_VERSION;
--- func.h
+++ func.h
@@ -1,7 +1,9 @@
 #ifndef FUNC_H
 #define FUNC_H
+
 #include <string>
 
 std::string getversion();
 extern int x; 
+
 #endif
