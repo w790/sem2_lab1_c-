298bb12b55699df2903fae62d99391c0b264c491
U-DESKTOP-J0QPH5V\Паха <pasha_w790@bk.ru>
2025-09-05 17:07:44 +0700

    fix bugs:version failed6

--- func.cpp
+++ func.cpp
@@ -3,5 +3,11 @@
 
 int x = 0;
 std::string getversion(){
+    #ifdef PROJECT_VERSION
+    std::cout << "DEBUG: PROJECT_VERSION macro is defined as: " << PROJECT_VERSION << std::endl;
     return PROJECT_VERSION;
+    #else
+    std::cout << "DEBUG: PROJECT_VERSION macro is NOT defined" << std::endl;
+    return "1.0.0";
+    #endif
 }
